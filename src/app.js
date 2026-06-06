8bf86d269a22d4e8518e3a7c7f224d1f1df56a08
PlemRuckol <plemruckol9609@gmail.com>
2026-06-06 18:09:05 +0900

    feat: create express server and concerts api

--- src/app.js
+++ src/app.js
@@ -3,23 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const concerts = [
+    {
+        id: 1,
+        title: "뮤지컬 레미제라블",
+        date: "2026-07-01",
+    },
+    {
+        id: 2,
+        title: "Jazz Night",
+        date: "2026-07-15",
+    },
+];
+
 app.get("/", (req, res) => {
   res.send("Ticketing Server Start!");
 });
 
 app.get("/concerts", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      title: "뮤지컬 레미제라블",
-      date: "2026-07-01"
-    },
-    {
-      id: 2,
-      title: "Jazz Night",
-      date: "2026-07-15"
-    }
-  ]);
+  res.json(concerts);
 });
 
 app.listen(PORT, () => {
