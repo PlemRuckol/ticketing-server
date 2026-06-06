963a3b6861e4f92651d067c4c8a5c1b90ee1785f
PlemRuckol <plemruckol9609@gmail.com>
2026-06-06 18:32:23 +0900

    feat: setup express server and concerts api

---
+++ notes/day01.md
@@ -0,0 +1,23 @@
+# Day01
+
+## 완료
+
+- GitHub 저장소 생성
+- Express 설치
+- 서버 실행 성공
+- 공연 목록 API 구현
+
+## 배운 점
+
+- Express는 HTTP 요청을 처리하는 프레임워크
+- app.get()으로 GET 요청 처리 가능
+
+## 문제 해결
+
+### package.json 오류
+
+원인:
+빈 package.json
+
+해결:
+npm init -y
--- src/app.js
+++ src/app.js
@@ -24,6 +24,20 @@ app.get("/concerts", (req, res) => {
   res.json(concerts);
 });
 
+app.get("/concerts/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const concert = concerts.find(c => c.id === id);
+
+    if (!concert){
+        return res.status(404).json({
+            message: "Concert not found"
+        });
+    }
+
+    res.json(concert);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
